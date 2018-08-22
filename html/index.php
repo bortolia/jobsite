4a794f42bdeac57507d41dfd38c218efeed8c51c
Angelo <bortolia@uwindsor.ca>
2018-08-22 08:12:22 -0400

    Add existing file

--- html/index.php
+++ html/index.php
@@ -305,42 +305,7 @@ elseif($_SESSION['usercode'] == "2"){
             <textarea class="form-control" id="jobDescription" name="jobDescription" rows="8" placeholder="Job Description" form="newJobPost" required></textarea>
           </div>
         </div>
-<!--        <fieldset class="form-group">
-          <div class="row">
-            <legend class="col-form-label col-sm-2 pt-0">Radios</legend>
-            <div class="col-sm-10">
-              <div class="form-check">
-                <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios1" value="option1" checked>
-                <label class="form-check-label" for="gridRadios1">
-                  First radio
-                </label>
-              </div>
-              <div class="form-check">
-                <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios2" value="option2">
-                <label class="form-check-label" for="gridRadios2">
-                  Second radio
-                </label>
-              </div>
-              <div class="form-check disabled">
-                <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios3" value="option3" disabled>
-                <label class="form-check-label" for="gridRadios3">
-                  Third disabled radio
-                </label>
-              </div>
-            </div>
-          </div>
-        </fieldset>
-        <div class="form-group row">
-          <div class="col-sm-2">Checkbox</div>
-          <div class="col-sm-10">
-            <div class="form-check">
-              <input class="form-check-input" type="checkbox" id="gridCheck1">
-              <label class="form-check-label" for="gridCheck1">
-                Example checkbox
-              </label>
-            </div>
-          </div>
-        </div>-->
+
         <div class="form-group row text-center">
           <div class="col-sm-12">
             <button type="submit" class="btn btn-primary col-sm-4 col-md-2" name="jobSubmit">Post Job+</button>
@@ -439,36 +404,6 @@ _END;
   }
 ?>
 
-<!-- OLD TEST COLUMNS
-      <div class="row">
-        
-        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
-          <h4>Column 1</h4>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.In pellentesque massa placerat duis.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. In pellentesque massa placerat duis.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. In pellentesque massa placerat duis.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. In pellentesque massa placerat duis.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. In pellentesque massa placerat duis.
-          <br><br>
-        </div>
-        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
-          <h4>Column 2</h4>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. In pellentesque massa placerat duis.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. In pellentesque massa placerat duis.Lorem ipsum dolor sit amet, consectetur adipiscing elit, Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. In pellentesque massa placerat duis.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. In pellentesque massa placerat duis.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. In pellentesque massa placerat duis.
-          <br><br>
-        </div>
-        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
-          <h4>Column 3</h4>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. In pellentesque massa placerat duis.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. In pellentesque massa placerat duis.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. In pellentesque massa placerat duis.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-          <br><br>
-        </div>
-        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
-          <h4>Column 4</h4>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. In pellentesque massa placerat duis.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. In pellentesque massa placerat duis.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. In pellentesque massa placerat duis.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. In pellentesque massa placerat duis.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. In pellentesque massa placerat duis. In pellentesque massa placerat duis.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. In pellentesque massa placerat duis.
-          <br><br>
-        </div> 
-      <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12 d-block d-sm-none">
-          <h4>Column 5 SECRET</h4>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. In pellentesque massa placerat duis.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. In pellentesque massa placerat duis.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. In pellentesque massa placerat duis.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. In pellentesque massa placerat duis.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. In pellentesque massa placerat duis.
-          <br><br>
-        </div>              
-      </div>
--->      
     </div>
 <?php
     function get_post($conn, $var){
--- html/jobview.php
+++ html/jobview.php
@@ -8,9 +8,60 @@
   if ($conn->connect_error) die($conn->connect_error);
 
   $userId = $_SESSION['userId'];
+  $fname = $_SESSION['fname'];
+  $lname = $_SESSION['lname'];
+  $email = $_SESSION['email'];
 
   $postId = $_GET['postId'];
-  
+  $_SESSION['postId'] = $postId;
+  $postId = $_SESSION['postId'];
+
+  if($_SESSION['usercode'] == "1"){
+
+  	$checkQuery = "SELECT * FROM Job_applications WHERE userId = '$userId' AND postId = '$postId'";
+  	$checkResult = $conn->query($checkQuery);
+  	if (!$checkResult) die($conn->error);
+  	$checkRows = $checkResult->num_rows;
+  	$checkResult->data_seek(0);
+  	$checkRow = $checkResult->fetch_array(MYSQLI_NUM);
+
+
+  	$resumeIdQuery = "SELECT resumeId FROM Resume WHERE userId = '$userId'";
+  	$resumeIdResult = $conn->query($resumeIdQuery);
+  	if (!$resumeIdResult) die($conn->error);
+  	$resumeIdRows = $resumeIdResult->num_rows;
+  	$resumeIdResult->data_seek(0);
+  	$resumeIdRow = $resumeIdResult->fetch_array(MYSQLI_NUM);
+  	$resumeId = $resumeIdRow[0];
+
+
+  	if(isset($_POST['applied'])){
+  		/*echo $postId;
+  		echo $resumeId;
+  		echo $fname;
+  		echo $lname;
+  		echo $email;
+  		echo $userId;*/
+		$applicStmt = $conn->prepare("INSERT INTO Job_applications (postId, resumeId, fname, lname, email, userId) VALUES (?,?,?,?,?,?)");
+	  	$applicStmt->bind_param("iisssi", $postId, $resumeId, $fname, $lname, $email, $userId);
+	  	$applicStmt->execute();
+
+	  	if(!$applicStmt->error){
+	  		echo "<script>window.location.href ='jobview.php?postId=$postId';</script>";
+	  	}
+
+  	}
+//do this after button
+/*  	$applicStmt = "INSERT INTO Job_applications (postId, resumeId, fname, lname, email, userId) VALUES (?,?,?,?,?,?)";
+  	$applicStmt->bind_param("iisssi", $postId, $resumeId, $fname, $lname, $email, $userId);
+  	$applicStmt->execute();
+
+  	if(!$applicStmt->error){
+  		echo 'yeah';
+  	}*/
+
+  }
+
   if(isset($_GET['postId'])){
   	
   	$jobViewQuery = "SELECT title, company, description, province, city FROM Job_posting WHERE postId='$postId'";
@@ -23,6 +74,7 @@
   	$jobViewResult->data_seek(0);
     $jobViewRow = $jobViewResult->fetch_array(MYSQLI_NUM);
     $formatDesc = preg_replace('/\v+|\\\r\\\n/','<br/>',$jobViewRow[2]);
+
   }
 
 ?>
@@ -75,8 +127,42 @@
       <div class="pb-2 mt-4 mb-2 border-bottom">
         <h1>Hello, <?php echo ' '.$_SESSION['fname'].'!';?></h1>
       </div>
-	  <div class="row" style="padding-top: 30px; padding-bottom: 30px;">
 <?php
+	if($_SESSION['usercode'] == "1"){	
+		if($checkRows <= 0){
+			
+echo <<<_END
+<div class="row">
+<form action="jobview.php?postId=$postId" method="post" class="text-center" style="padding-left: 15px;">
+<button name="applied" type="submit" value="applied" class="btn btn-outline-primary">Apply To This Job</button><br><br>
+</form>
+</div>
+_END;
+		}
+		else
+			echo '<p class="text-success h5">*Thank you for your application*</p>';
+	}
+
+?>
+
+	  <div class="row" style="padding-top: 0px; padding-bottom: 30px;">
+<?php
+/*
+	if($_SESSION['usercode'] == "1"){	
+		if($checkRows <= 0){
+			
+echo <<<_END
+<div class="row>
+<form action="jobview.php" method="post" class="text-center">
+<button name="applied" value="applied" class="btn btn-outline-success">Apply To This Job</button><br><br>
+</form>
+</div>
+_END;
+		}
+	}
+*/
+
+
 	/*
 	$jobViewResult->data_seek(0);
     $jobViewRow = $jobViewResult->fetch_array(MYSQLI_NUM);
@@ -89,25 +175,63 @@ echo <<<_END
 	<p class="h4">$jobViewRow[1]</p>
 	<p class="h6">Location: $jobViewRow[4], $jobViewRow[3]</p>
 	<br>
-	<p class="h6">$formatDesc</p>
+	<p class="h6" style="margin-bottom: 30px;">$formatDesc</p>
 	</div>
 _END;
 ?>
 <?php
 if($_SESSION['usercode'] == "2"){
+
+	$tableQuery = "SELECT fname, lname, email, resumeId, userId FROM Job_applications WHERE postId ='$postId'";
+	$tableResult = $conn->query($tableQuery);
+
+	if (!$tableResult) die($conn->error);
+
+	$tableRows = $tableResult->num_rows;
+
 ?>
 	<div class="col-md-5">
+		<p class="h2">Job Applicants</p>
 		<table class="table table-striped" style="padding-left: 0px; padding-right: 0px;">
 		  <thead>
 		    <tr>
 		      <th scope="col">#</th>
 		      <th scope="col">First</th>
 		      <th scope="col">Last</th>
-		      <th scope="col">Handle</th>
+		      <th scope="col">Email</th>
+		      <th scope="col">Resume</th>
 		    </tr>
 		  </thead>
 		  <tbody>
-		    <tr>
+
+<?php
+	
+	for($i = 0; $i < $tableRows; $i++){
+
+		$tableResult->data_seek($i);
+    	$tableRow = $tableResult->fetch_array(MYSQLI_NUM);
+    	$num = $i+1;
+    	$userIdForResume = $tableRow[4];
+
+		echo <<<_END
+		<tr>
+		<th scope="col">$num</th>
+		<td>$tableRow[0]</td>
+		<td>$tableRow[1]</td>
+		<td>$tableRow[2]</td>
+		<td>
+        <form action="showfile.php" method="get" class="text-center" style="max-height: 40px;">
+          <button name="id" value="$userIdForResume" class="btn btn-outline-info">Download</button><br><br>
+        </form>
+		</td>
+		</tr>
+_END;
+	}
+
+
+?>
+
+<!--		    <tr>
 		      <th scope="row">1</th>
 		      <td>Mark</td>
 		      <td>Otto</td>
@@ -124,7 +248,7 @@ if($_SESSION['usercode'] == "2"){
 		      <td>Larry</td>
 		      <td>the Bird</td>
 		      <td>@twitter</td>
-		    </tr>
+		    </tr>-->
 		  </tbody>
 		</table>
 	</div>
